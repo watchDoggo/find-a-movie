70968926e5419a38383f0283f2a1784da5b86c69
watchDoggo <michele.gaeta.90@gmail.com>
2018-11-23 17:31:46 +0100

    Initial commit

---
+++ httpServer.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+//var express = require('express');
+
+
+//var app = express();
+
+var hostname = '127.0.0.1';
+var port = 3000;
+
+
+var server = http.createServer((req, res) => {
+
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+
+	var page = url.parse(req.url).pathname;
+
+	if (page == "/00"){
+
+		fs.readFile('firstHTML.html', (err, html) => {
+
+		res.write(html);
+		res.end();
+
+		});
+	}
+	else{
+
+		fs.readFile('secondHTML.html', (err, html) => {
+
+			res.write(html);
+			res.end();
+		});
+	}
+});
+
+server.listen(port, hostname);
+
+console.log("Server started on port " + port);
+
+
+
